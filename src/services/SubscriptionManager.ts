cfa3ef3bd28a91bfb52140ef8cbe7bb450320e89
Thomas Zwarts <thomaszwarts@gmail.com>
2020-04-30 14:18:14 +0200

    confusing mixing between await and .then/.catch streamlined

--- src/services/SubscriptionManager.ts
+++ src/services/SubscriptionManager.ts
@@ -20,36 +20,26 @@ export class SubscriptionManager implements IService {
         this.isRunning = true;
         const keys = await this.redis.keys("*");
         const now = Date.now() / 1000;
-        const subscribes = [];
+        const channelRepository = getConnection().getRepository(Channel);
         for (const key of keys) {
+            // wait a second between requests to not get throttled
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
             const time = Number(await this.redis.get(key));
             // if the difference is higher than 0, it means it is in the past so we should resubscribe!
             if (time - now > 0) {
                 console.log("re-subscribing", time, now, time - now);
-                subscribes.push(new Promise(async (resolve) => {
-                    getConnection()
-                        .getRepository(Channel)
-                        .findOne({
-                            where: {
-                                id: key
-                            }
-                        })
-                        .then(channel => {
-                            if (!channel || !channel.channel_id) throw new Error("channel without channel_id that needs to be resubscribed, crashing..");
-                            this.youtubeNotifier.notifier.subscribe(channel.channel_id);
-                            this.redis.set(key, String((Date.now() / 1000) + (432000 - 300)))
-                                .then(x => {
-                                    resolve();
-                                })
-                                .catch(err => console.log(err));
-                        })
-                        .catch(err => console.log(err));
-                }));
-                // wait a second between requests to not get throttled
-                await new Promise(resolve => setTimeout(() => resolve(), 1000));
+                const channel = await channelRepository
+                    .findOne({
+                        where: {
+                            id: key
+                        }
+                    });
+                if (!channel || !channel.channel_id) throw new Error("channel without channel_id that needs to be resubscribed, crashing..");
+                this.youtubeNotifier.notifier.subscribe(channel.channel_id);
+                await this.redis.set(key, String((Date.now() / 1000) + (432000 - 300)))
             }
         }
-        await Promise.all(subscribes);
         this.isRunning = false;
     };
 
